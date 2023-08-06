0dd2b071458dacb60aad94b59a8eebe56dec3652
Thaua <thaua19felipe@gmail.com>
2023-08-06 13:44:11 -0300

    Criando schema News

---
+++ src/models/News.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const NewsSchema = new mongoose.Schema({
+    title:  {
+        type: String,
+        require: true
+    },
+    text: {
+        type: String,
+        require: true
+    },
+    banner: {
+        type: String,
+        require: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    likes: {
+        type: Array,
+        required: true
+    },
+    comments: {
+        type: Array,
+        required: true
+    },
+})
+
+const News = mongoose.model('News', NewsSchema)
+module.exports = News
