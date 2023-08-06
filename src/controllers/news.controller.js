fd95cd4bc4441c4cf8beb8a3ce0718f18ab8bff4
Thaua <thaua19felipe@gmail.com>
2023-08-06 14:21:58 -0300

    criação efetiva da noticia no banco

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -10,7 +10,7 @@ const create = async (req, res) => {
         } 
 
         await newsService.createService({
-            title, text, banner, id: "objectIdFake"
+            title, text, banner, user:"64ce863383adf025bcc91f1f"
         })
 
         res.status(201).send("criar noticia")
@@ -24,7 +24,13 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) => {
 
-    res.status(201).send("noticias")
+    const news = await newsService.findAllService()
+
+    if(news.length === 0){
+        return res.status(400).send({ message: "There are no registered news"})
+    }
+
+    res.status(201).send(news)
 
 }
 
