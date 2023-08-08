95147cf26b75aa71a41dadca53b3bbe94d7e66af
Thaua <thaua19felipe@gmail.com>
2023-08-08 15:51:23 -0300

    Documentação da API no Swegger

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const dotenv = require('dotenv').config()
 const authRoute = require('./src/routes/auth.route')
 const userRoute = require('./src/routes/user.route')
 const newsRoute = require('./src/routes/news.route')
+const swaggerRoute = require('./src/routes/swegger.route')
 
 const port = process.env.PORT || 3333
 
@@ -14,6 +15,7 @@ app.use(express.json())
 app.use('/user', userRoute)
 app.use('/auth', authRoute)
 app.use('/news', newsRoute)
+app.use('/doc', swaggerRoute)
 
 app.listen(port, () => console.log(`Servidor Online na porta ${port}`)
 )
