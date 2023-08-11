c495bd4f1371ade7189f9043e49eea447ff51971
Thaua <thaua19felipe@gmail.com>
2023-08-11 10:44:01 -0300

    Configuração CORS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const app = express()
 const connectDatabase = require('./database/db')
 const dotenv = require('dotenv').config()
 
+const cors = require('cors')
+
 const authRoute = require('./routes/auth.route')
 const userRoute = require('./routes/user.route')
 const newsRoute = require('./routes/news.route')
@@ -11,6 +13,14 @@ const swaggerRoute = require('./routes/swegger.route')
 const port = process.env.PORT || 3333
 
 connectDatabase()
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_ACESS)  
+
+    res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,DELETE,POST")
+
+    app.use(cors())
+    next()
+})
 app.use(express.json())
 app.use('/user', userRoute)
 app.use('/auth', authRoute)
